63ef46fc3655f322ed56fc7f883f83f629d1556c
Menashe <moses2787@gmail.com>
2019-02-25 01:24:02 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var addAlbumButton = document.querySelector('.album-button');
+var captionInput = document.querySelector('#caption-input');
+var chooseFileButton = document.querySelector('.inputfile');
+var searchButton = document.querySelector('.search-button');
+var searchInput = document.querySelector('.search-input');
+var titleInput = document.querySelector('#title-input');
+var viewFavoritesButton = document.querySelector('.favorites-button');
+
